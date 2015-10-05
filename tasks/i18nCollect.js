dc38e41743c0f740042db591a4a08fab8cb881d6
Denys <webschik@gmail.com>
2015-10-05 09:27:19 +0200

    Create i18nCollect.js

---
+++ tasks/i18nCollect.js
@@ -0,0 +1,91 @@
+var _ = require('lodash');
+
+module.exports = function (grunt) {
+    grunt.registerMultiTask('i18nCollect', 'Collect i18n data', function () {
+        var options = this.options({
+            locales: [],
+            method: ''
+        });
+
+        if (!options.method) {
+            grunt.log.warn('Method is not specified.');
+            return false;
+        }
+
+        this.files.forEach(function (f) {
+            var values = {};
+            var dest = f.dest.replace(/\/$/, '') + '/';
+
+            f.src.filter(function (filepath) {
+                // Warn on and remove invalid source files (if nonull was set).
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }).forEach(function (filepath) {
+                var src;
+                var regExp = new RegExp(options.method + '\\(([^)]+)\\)', 'g');
+                var res;
+
+                try {
+                    src = grunt.file.read(filepath);
+                } catch (e) {}
+
+                res = src && regExp.exec(src);
+                while (res) {
+                    var parts = res[1].replace(/^['`"]/, '').replace(/['`"]$/, '').split('.');
+                    var len = parts.length;
+                    var root = values;
+                    parts.some(function (part, i) {
+                        if (/^[\$\{]{2}/.test(part)) {
+                            return true;
+                        }
+
+                        if (i === len - 1) {
+                            root[part] = '';
+                        } else {
+                            root = root[part] = root[part] || {};
+                        }
+
+                        return false;
+                    });
+
+                    res = regExp.exec(src);
+                }
+            });
+
+            var banner = '';
+            var footer = '\nexport default {\n';
+            options.locales.forEach(function (locale) {
+                var path = dest + locale + '.js';
+                var src;
+
+                try {
+                    src = grunt.file.read(path);
+                } catch (e) {}
+
+                if (src) {
+                    src = src.replace(/^[^\{]+/, '').replace(/[^\}]+$/, '');
+                }
+                src = src || '{}';
+
+                var data = eval('(' + src + ')');
+                data = _.merge({}, values, data);
+                grunt.file.write(path, 'export default ' +
+                    JSON.stringify(data)
+                        .replace(/\{/g, '{\n')
+                        .replace(/",/g, '",\n')
+                        .replace(/,"/g, ',\n"')
+                        .replace(/}/g, '\n}') +
+                    ';');
+                banner += 'import ' + locale + ' from \'./' + locale + '\';\n';
+                footer += locale + ':' + locale + ',';
+            });
+
+            footer = footer.replace(/,$/, '') + '};';
+            grunt.file.write(dest + 'export.js', banner + footer);
+        });
+    });
+};
